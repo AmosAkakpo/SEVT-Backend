734fd89226b2ed82c08e7ceb962af650cec31aed
Amos Akakpo <amoseakakpo@example.com>
2025-09-08 17:37:33 +0400

    updating branches info modification

--- brancheRoutes.js
+++ brancheRoutes.js
@@ -1,81 +1,3 @@
-// const express = require("express")
-// const database = require("./connect")
-// const ObjectId = require("mongodb").ObjectId
-
-// let branchRoutes = express.Router()
-
-// //retrieve all 
-// //http://localhost:3000/branches
-// branchRoutes.route("/branches").get(async (request,response)=>{
-//     let db = database.getDb()
-//     let data = await db.collection("branches").find({}).toArray()
-
-//     if(data.length>0){
-//         response.json(data)
-//     }else {
-//         throw new Error("Data was not found :(")
-//     }
-// })
-
-// //retrieve one
-// //http://localhost:3000/branches/12345
-// branchRoutes.route("/branches/:id").get(async (request,response)=>{
-//     let db = database.getDb()
-//     let data = await db.collection("branches").findOne({_id: new ObjectId(request.params.id)})
-
-//     if(Object.keys(data).length>0){
-//         response.json(data)
-//     }else {
-//         throw new Error("Data was not found :(")
-//     }
-// })
-
-
-// //create one
-// //http://localhost:3000/branches
-// branchRoutes.route("/branches").post(async (request,response)=>{
-//     let db = database.getDb()
-//     let mongoObject = {
-//         branchName : request.body.branchName ,
-//         branchMainPastor: request.body.branchMainPastor,
-//         branchLocation:request.body.branchLocation,
-//         branchType:request.body.branchType,
-//         branchCreationDate:request.body.branchCreationDate,
-//     }
-//     let data = await db.collection("branches").insertOne(mongoObject)
-//     response.json(data)
-// })
-
-
-// //Update one
-// //http://localhost:3000/branches
-// branchRoutes.route("/branches/:id").post(async (request,response)=>{
-//     let db = database.getDb()
-//     let mongoObject = {
-//         $set: {
-//             branchName : request.body.branchName ,
-//             branchMainPastor: request.body.branchMainPastor,
-//             branchLocation:request.body.branchLocation,
-//             branchType:request.body.branchType,
-//             branchCreationDate:request.body.branchCreationDate
-//         }
-//     }
-//     let data = await db.collection("branches").updateOne({_id: new ObjectId(request.params.id)},mongoObject)
-//     response.json(data)
-// })
-
-// //Delete one
-// //http://localhost:3000/branches
-// branchRoutes.route("/branches/:id").delete(async (request,response)=>{
-//     let db = database.getDb()
-//     let data = await db.collection("branches").deleteOne({_id: new ObjectId(request.params.id)})
-
-//     response.json(data)
-// })
-
-
-// module.exports = branchRoutes
-
 const express = require("express");
 const database = require("./connect");
 const { ObjectId } = require("mongodb");
@@ -134,37 +56,44 @@ branchRoutes.route("/").post(async (req, res) => {
     res.status(500).json({ error: "Server error" });
   }
 });
-
-// PUT update branch
+// PUT update branch (partial update supported)
 branchRoutes.route("/:id").put(async (req, res) => {
   try {
     if (!isValidObjectId(req.params.id)) {
       return res.status(400).json({ error: "Invalid ID format" });
     }
+
     const db = database.getDb();
-    const updateData = {
-      $set: {
-        branchName: req.body.branchName,
-        branchMainPastor: req.body.branchMainPastor,
-        branchmptelephone: req.body.branchmptelephone, // <-- NEW FIELD
-        branchLocation: req.body.branchLocation,
-        branchType: req.body.branchType,
-        branchCreationDate: req.body.branchCreationDate,
-      }
-    };
-    const data = await db.collection("branches").updateOne(
+
+    // Build update object dynamically
+    const updateFields = {};
+    if (req.body.branchName !== undefined) updateFields.branchName = req.body.branchName;
+    if (req.body.branchMainPastor !== undefined) updateFields.branchMainPastor = req.body.branchMainPastor;
+    if (req.body.branchmptelephone !== undefined) updateFields.branchmptelephone = req.body.branchmptelephone;
+    if (req.body.branchLocation !== undefined) updateFields.branchLocation = req.body.branchLocation;
+    if (req.body.branchType !== undefined) updateFields.branchType = req.body.branchType;
+    if (req.body.branchCreationDate !== undefined) updateFields.branchCreationDate = req.body.branchCreationDate;
+
+    if (Object.keys(updateFields).length === 0) {
+      return res.status(400).json({ error: "No valid fields to update" });
+    }
+
+    const result = await db.collection("branches").updateOne(
       { _id: new ObjectId(req.params.id) },
-      updateData
+      { $set: updateFields }
     );
-    if (data.matchedCount === 0) {
+
+    if (result.matchedCount === 0) {
       return res.status(404).json({ error: "Branch not found" });
     }
-    res.json(data);
+
+    res.json({ message: "Branch updated", result });
   } catch (error) {
     console.error(error);
     res.status(500).json({ error: "Server error" });
   }
 });
+
 
 
 // DELETE branch
